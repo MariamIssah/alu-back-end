dab30e217538bbb2e2715b7def09732650d8ae7c
MariamIssah <m.issah1@alustudent.com>
2024-06-26 19:36:08 -0700

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,7 +10,6 @@ from sys import argv
 if __name__ == "__main__":
     # Get the user ID from the command-line argument
     user_id = argv[1]
-    
     # Step 1: Retrieve user information from the JSONPlaceholder API.
     request_employee = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{user_id}'
@@ -18,21 +17,17 @@ if __name__ == "__main__":
     employee = json.loads(request_employee.text)
     employee_name = employee.get("name")
     userName = employee.get("username")
-
     # Step 2: Retrieve the user's tasks from the API.
     request_todos = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
     )
     tasks = {}
     employee_todos = json.loads(request_todos.text)
-
     # Step 3: Create a list of tasks.
     for dictionary in employee_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
-
     # Step 4: Generate the CSV filename based on the user's ID.
     USER_ID = user_id
-
     # Create and write the data to a CSV file.
     with open(f'{USER_ID}.csv', 'w', encoding="UTF8", newline='') as user:
         writer = csv.writer(user, delimiter=',', quoting=csv.QUOTE_ALL)
